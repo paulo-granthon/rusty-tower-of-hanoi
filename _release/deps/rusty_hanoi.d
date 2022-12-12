c8521adc0f83f478d3dfea5c007ba465841d9ca4
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2022-12-12 20:41:00 -0300

    update .gitignore

--- _release/deps/rusty_hanoi.d
+++
@@ -1,7 +0,0 @@
-C:\Users\PICHAU\Documents\__dev\rusty_hanoi\target\release\deps\rusty_hanoi.exe: src\main.rs
-
-C:\Users\PICHAU\Documents\__dev\rusty_hanoi\target\release\deps\rusty_hanoi.d: src\main.rs
-
-src\main.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.1.0
--- _release/rusty_hanoi.d
+++
@@ -1,1 +0,0 @@
-C:\Users\PICHAU\Documents\__dev\rusty_hanoi\target\release\rusty_hanoi.exe: C:\Users\PICHAU\Documents\__dev\rusty_hanoi\src\main.rs
